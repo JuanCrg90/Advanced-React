df20faae1a56cfe3066b36e172112d50eb2c9d97
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2019-12-02 14:09:36 -0600

    Add item mutation and query

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,13 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+  // Prisma bindings way
+  items: forwardTo('db'),
+  // Manual Way
+  // async items(parent, args, ctx, info) {
+  //   const items = await ctx.db.query.items();
+  //   return items;
+  // }
+};
 
 module.exports = Query;
