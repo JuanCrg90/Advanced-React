915228c97fcd0ee7f2d361a4e7639b4e281b1a74
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2019-12-11 11:23:02 -0600

    Add Update item feature

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -3,6 +3,7 @@ const { forwardTo } = require('prisma-binding');
 const Query = {
   // Prisma bindings way
   items: forwardTo('db'),
+  item: forwardTo('db'),
   // Manual Way
   // async items(parent, args, ctx, info) {
   //   const items = await ctx.db.query.items();
